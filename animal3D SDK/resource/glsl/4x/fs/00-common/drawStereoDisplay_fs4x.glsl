cd2a618025630975ba32f694ac169d308e2dffdf
Daniel S. Buckstein <dbuckstein@users.noreply.github.com>
2026-04-10 09:09:22 -0400

    Implement barrel distortion.

--- animal3D SDK/resource/glsl/4x/fs/00-common/drawStereoDisplay_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/00-common/drawStereoDisplay_fs4x.glsl
@@ -32,9 +32,47 @@ uniform sampler2D uImage00;
 
 layout (location = 0) out vec4 rtFragColor;
 
+
+uniform vec4 uAxis;
+
+
+vec2 barrel(in vec2 c, in vec2 c0)
+{
+	vec2 dc  = c - c0;
+	float k1 = 1.0e-6;
+	float r2 = dot(dc, dc);
+	return c0 + dc * (1.0 + r2 * k1);
+}
+
+
+vec4 barrelStereo(in vec2 texcoord)
+{
+	vec4 col = vec4(0.0, 0.0, 0.0, 1.0);
+	if (texcoord.x < 0.5)
+	{
+		const vec2 c0_l = vec2(0.25, 0.50);
+		texcoord = barrel(texcoord * uAxis.xy, c0_l * uAxis.xy) * uAxis.zw;
+		if ((texcoord.x >= 0.0) && (texcoord.x < 0.5) &&
+			(texcoord.y >= 0.0) && (texcoord.y < 1.0))
+			col = texture(uImage00, texcoord);
+	}
+	else
+	{
+		const vec2 c0_r = vec2(0.75, 0.50);
+		texcoord = barrel(texcoord * uAxis.xy, c0_r * uAxis.xy) * uAxis.zw;
+		if ((texcoord.x >= 0.5) && (texcoord.x < 1.0) &&
+			(texcoord.y >= 0.0) && (texcoord.y < 1.0))
+			col = texture(uImage00, texcoord);
+	}
+	return col;
+}
+
+
 void main()
 {
-	// DUMMY OUTPUT: texture sample
-	vec4 col = texture(uImage00, vTexcoord_atlas.xy);
-	rtFragColor = col;
+	//// DUMMY OUTPUT: texture sample
+	//vec4 col = texture(uImage00, vTexcoord_atlas.xy);
+	//rtFragColor = col;
+
+	rtFragColor = barrelStereo(vTexcoord_atlas.xy);
 }
